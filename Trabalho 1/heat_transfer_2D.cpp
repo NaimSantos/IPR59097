38e1ebc8cf4e40f3efbf77b51ad3a7ab7ee40e2b
NaimSantos <naimsantos45@gmail.com>
2021-08-28 17:10:25 -0300

    Atualização da rotina principal e funções de correção de contornos

--- Trabalho 1/heat_transfer_2D.cpp
+++ Trabalho 1/heat_transfer_2D.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include <string>
+#include <iomanip>
 #include <algorithm> //std::fill
 #include <fstream>   //std::fstream
 
@@ -8,53 +9,65 @@
 
 void solver_finite_difference(std::vector<std::vector<double>>& T, const double r, const double gamma){
 	//Calcula os valores dos nós:
-	const auto N = A[0].size();
-	const auto M = A.size();
+	const auto N = T[0].size();
+	const auto M = T.size();
 	for (int i = 1; i < N-1; i++){
-		for(int j = 1; j < M -1; j++){
-			T[i][j] = r*(T[i+1][j] + T[i-1][j] + T[i][j+1] + T[i][j-1] - 4*T[i][j]) + T[i1][j] + gamma;
+		for (int j = 1; j < M-1; j++){
+			T[i][j] = r*(T[i+1][j] + T[i-1][j] + T[i][j+1] + T[i][j-1] - 4*T[i][j]) + T[i][j] + gamma;
 		}
 	}
 }
 
-void start_save_data(std::fstream& u_file, const std::vector<std::vector<double>>& T){
+void start_save_data(std::fstream& u_file, const std::vector<std::vector<double>>& A){
 	u_file << "Perfil de Temperatura\n";
-	u_file << "x y T\n";
+	u_file << std::setw(4) << "x " << "y " << "T\n";
 	const auto N = A[0].size();
 	const auto M = A.size();
 	for (int i = 0; i < N; i++){
-		for(int j = 0; j < M; j++){
-			//if (j == M-1)
-			//	u_file << T[i][j];
-			//else
-				u_file << T[i][j] << " ";
+		for (int j = 0; j < M; j++){
+			if (j == M-1)
+				u_file << std::setprecision(3) << std::setw(8) << A[i][j];
+			else
+				u_file << std::setprecision(3) << std::setw(8) << A[i][j] << " ";
 		}
-		u_file << "\n\n";
+		u_file << "\n";
 	}
-
+	u_file << "\nFim do inicio\n";
 }
 
-void resume_save_data(std::fstream& u_file, const std::vector<std::vector<double>>&){
+void resume_save_data(std::fstream& u_file, const std::vector<std::vector<double>>& A){
+	static int count = 0;
 	const auto N = A[0].size();
 	const auto M = A.size();
+	//std::cout << "\ncount = " << count ;
+	u_file << "\n";
+	//u_file << "\ncount = " << count << ", N = " << N << ", M = " << M << std::endl;
 	for (int i = 0; i < N; i++){
-		for(int j = 0; j < M; j++){
-				u_file << T[i][j] << " ";
+		for (int j = 0; j < M; j++){
+			//std::cout << A[i][j] << " ";
+			if (j == M-1)
+				u_file << std::setw(8) << A[i][j] ;
+			else
+				u_file << std::setw(8) << A[i][j] << " ";
 		}
-		u_file << "\n\n";
+		u_file << "\n";
 	}
+	count++;
 }
 
-void fix_bounds_newman(std::vector<std::vector<double>>& A){
+void fix_bounds(std::vector<std::vector<double>>& A, const double valueX, const double valueY){
 	auto m = A.size();
 	auto n = A[0].size();
-	
-}
 
-void fix_bounds_dirichillet(std::vector<std::vector<double>>& A, const double valueX, const double valueY){
-	auto m = A.size();
-	auto n = A[0].size();
-	std::fill(A[0].begin(), A[0].end(), valueX);
-	for (int i = 0; i < m; i++)
-		A[m-1][i] = valueY;
+	// Aresta superior:
+	std::fill(A[0].begin(), A[0].end(), valueX); 
+	// Aresta direita
+	for (int k = 0; k < m; k++)
+		A[k][0] = A[k][1];
+	// Aresta esquerda:
+	for (int k = 0; k < m; k++)
+		A[k][n-1] = valueY;
+	// Aresta inferior:
+	for (int k = 0; k < m; k++)
+		A[m-1][k] = A[m-2][k];
 }
--- Trabalho 1/main.cpp
+++ Trabalho 1/main.cpp
@@ -8,44 +8,51 @@
 #include "heat_transfer_2D.h"
 
 // Variáveis do problema:
-constexpr double kappa {0.6};                          // coeficiente de difusividade térmica
-constexpr double rho {1000};                           // massa específica
-constexpr double cp {0.6};                             // calor específico
+constexpr double kappa {15.1};                          // coeficiente de difusividade térmica
+constexpr double rho {8050};                            // massa específica
+constexpr double cp {480};                            // calor específico
+constexpr double g {100000};                           // geração interna
 // Variáveis do domínio:
-constexpr double L {1.0};                              // tamanho em x
-constexpr double H {1.0};                              // tamanho em y
-constexpr int Nx {10};                                 // número de nós em x
-constexpr int Ny {10};                                 // número de nós em y
+constexpr double L {0.05};                              // tamanho em x
+constexpr double H {0.05};                              // tamanho em y
+constexpr int Nx {9};                                 // número de nós em x
+constexpr int Ny {9};                                 // número de nós em y
 constexpr auto dx = L/(Nx-1);                          // comprimento da célula
 constexpr auto dy = L/(Ny-1);                          // altura da célula
 // Variáveis da simulação:
-constexpr double T_init {20.0};                        // temperatura inicial da placa
+constexpr double T_init {25.0};                       // temperatura inicial da placa
 constexpr double ti {0.0};                             // tempo inicial da simulação
-constexpr double tf {100.0};                           // tempo final da simulação
-constexpr int nsteps {4096};                           // número de passos de tempo
+constexpr double tf {1000.0};                           // tempo final da simulação
+constexpr int nsteps {4096*4};                            // número de passos de tempo
 constexpr auto dt = (tf-ti)/nsteps;                    // tamanho do passo de tempo
 
 int main(int argc, char* argv[]){
 
 	// Variáveis auxiliares do problema:
 	constexpr auto gamma = g*dt/(rho*cp);
+	constexpr auto alfa = kappa/(rho*cp);
 	constexpr auto r = kappa*dt/(rho*cp*dx*dx);
+	constexpr auto stab = (dx*dx) / (4*alfa);
+	std::cout << "stability = " << stab << "\ndt = "<< dt << ", dx = " << dx << ", r = " << r << ", gamma = " << gamma << ", alfa = " << alfa << std::endl;
+
 
 	// Malha:
 	std::vector<std::vector<double>> T(Ny, std::vector<double>(Nx, T_init));
+	fix_bounds(T, 100, 100);
 	// Arquivo de saida:
-	std::fstream print_start {"2D_Heat.dat", std::ios::out|std::ios::trunc};
-	std::fstream print_resume {"2D_Heat.dat", std::ios::out|std::ios::app};
+	std::fstream save_1 {"2D_Heat.dat", std::ios::out|std::ios::trunc};
+	std::fstream save_2 {"2D_Heat.dat", std::ios::out|std::ios::app};
 
-	/*
-	//Inicio da execução:
+	// Inicio da execução:
+	start_save_data(save_1, T);
 
-	//Manipulador do arquivo de saida:
-	std::string arquivo {"Data_"};
-	auto tempo = std::to_string(3.1415);
-	std::fstream printer {arquivo + tempo + ".dat", std::ios::out|std::ios::trunc};
-	std::fstream {"2D_Heat.dat", std::ios::out|std::ios::app};
-	start_save_data(printer);
-	*/
+	for (int step = 0; step < nsteps; step++){
+		solver_finite_difference(T, r, gamma);
+		fix_bounds(T, 100, 100);
+		if (step%100==0)
+			resume_save_data(save_2, T);
+	}
+
+
 	std::cout << "\nExecution reached the end" << std::endl;
 }
