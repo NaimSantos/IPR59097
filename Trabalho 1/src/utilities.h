ed54ace7cdea910e6dea1f840c1ea4e390fb8bf9
NaimSantos <naimsantos45@gmail.com>
2021-09-07 15:20:28 -0300

    Adição de registro de tempo via objeto CustomTimer

--- Trabalho 1/src/utilities.h
+++ Trabalho 1/src/utilities.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <cmath>
 #include <iostream>
+#include <chrono>
 
 template<typename T>
 bool is_diagonal_dom(const std::vector<std::vector<T>>& A){
@@ -93,3 +94,29 @@ void print_array_1D(const std::vector<T> M){
 	}
 	std::cout << '\n';
 }
+
+class CustomTimer
+{
+	public:
+		CustomTimer(){
+			startpoint = std::chrono::steady_clock::now();
+		}
+		~CustomTimer(){
+			TimeDestructor();
+		}
+
+		void TimeDestructor(){
+			auto endpoint = std::chrono::steady_clock::now();
+
+			auto start = std::chrono::time_point_cast<std::chrono::milliseconds>(startpoint).time_since_epoch().count();
+			auto end = std::chrono::time_point_cast<std::chrono::milliseconds>(endpoint).time_since_epoch().count();
+
+			auto total = end - start;
+			double s = total*0.001;
+			
+			std::cout << "\nTempo decorrido:" << s << " segundos (" << total << " milisegundos)" << std::endl;
+		}
+	
+		private:
+			std::chrono::time_point<std::chrono::steady_clock> startpoint;
+};
