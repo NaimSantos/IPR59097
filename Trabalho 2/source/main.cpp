716ef9064d6ea81a6c5285c71791711cdd70ac9e
NaimSantos <naimsantos45@gmail.com>
2021-09-30 15:38:06 -0300

    Versão inicial da solução explícita

--- Trabalho 2/source/main.cpp
+++ Trabalho 2/source/main.cpp
@@ -6,13 +6,55 @@
 #include <string>
 #include <chrono>
 
-void GS_Solver(const std::vector<std::vector<T>>& A, std::vector<T>& B, std::vector<T>& X);
+void GS_Solver(const std::vector<std::vector<double>>& A, std::vector<double>& B, std::vector<double>& X);
+template <typename T> void linspace(std::vector<double>& V, int Num, T xf, T xi);
+void save_data_2D(const std::vector<std::vector<double>>& A, const std::string& filename);
 template <typename T>
-void linspace(std::vector<double>& V, const int Num, const T xf, const T xi);
+void print_array_2D(const std::vector<std::vector<T>> M);
+
+constexpr double kappa {0.6};                            // coeficiente de condutividade térmica
+constexpr double h {100.0};                               // coeficiente de troca de calor por convecção
+constexpr double cp {1200.0};                            // calor específico
+constexpr double rho {600.0};                            // massa específica
+constexpr double g {100000.0};                           // geração interna
+constexpr double T_inf{20.0};                            // temperatura do ambiente
+constexpr double T_0 {20};                               // temperatura inicial da placa
+constexpr double L {0.03};                               // comprimento da placa
+constexpr int N {11};                                    // número de nós em x
+constexpr double ti {0.0};                               // tempo inicial
+constexpr double tf {5.0};                              // tempo total de simulação
+constexpr double dt {1};                               // passo de tempo
+constexpr auto dx = L /(N - 1);                          // intervalo
+constexpr auto nsteps = static_cast<int>((tf - ti)/dt);  // número de passos de tempo
 
 int main(int argc, char* argv[]){
+	constexpr auto alpha = kappa / (rho * cp);
+	constexpr auto r = (alpha * dt)/(dx*dx);
+	constexpr auto gamma = (alpha * g * dt) / kappa;
+	constexpr auto mu = (2 * dx * h) / kappa;
 
+	std::cout << "alpha = " << alpha << std::endl;
+	std::cout << "r = " << r << std::endl;
+	std::cout << "gamma = " << gamma << std::endl;
+	std::cout << "mi = " << mu << std::endl;
 
+	std::vector<std::vector<double>> T(nsteps, std::vector<double>(N, T_0));  // matriz N x nsteps, para as temperaturas em todos os tempos
+	std::vector<double> Pos(N, 0.0);                                          // vetor com as posições avaliadas
+	linspace(Pos, N, L, 0.0);
+
+	print_array_2D(T);
+	for (int i = 1; i < nsteps; i++){
+		T[i][0] = -r*T[i][0] + 2*r*T[i][1] + gamma;
+		for (int j = 1; j < N-1; j++){
+			
+			T[i][j] = r*T[i][j-1] - r*T[i][j] + r*T[i][j+1] + gamma;
+			std::cout << "i = " << i << " j = " << j << "\tT = " << T[i][j] << std::endl;
+		}
+		T[i][N-1] = 2*r*T[i][N-2] - (r + mu)*T[i][N-1] + r*mu*T_inf + gamma;
+	}
+	const std::string file1 {"output_data.txt"};
+	save_data_2D(T, file1);
+	print_array_2D(T);
 	std::cout << "Execution reached the end" << std::endl;
 }
 
@@ -49,10 +91,35 @@ void GS_Solver(const std::vector<std::vector<double>>& A, std::vector<double>& B
 }
 
 template <typename T>
-void linspace(std::vector<double>& V, const int Num, const T xf, const T xi){
+void linspace(std::vector<double>& V, int Num, T xf, T xi){
 	auto h = (xf - xi) / (Num-1);
 	auto n = static_cast<int>(V.size());
 	for (int i = 0; i < n; i++){
 		V[i] = xi + i*h;
+	}
+}
+
+void save_data_2D(const std::vector<std::vector<double>>& A, const std::string& filename){
+	std::fstream saver {filename, std::ios::out|std::ios::trunc};
+	int n = A[0].size();
+	int m = A.size();
+	
+	for (int i = 0; i < m; i++){
+		for (int j = 0; j < n; j++){
+			saver << A[i][j] << ' ';
+		}
+		saver << '\n';
+	}
+}
+
+template <typename T>
+void print_array_2D(const std::vector<std::vector<T>> M){
+	auto nrow = M.size();
+	auto ncol = M[0].size();
+	for(int i = 0; i < nrow; i++){
+		for(int j = 0; j < ncol; j++){
+			std::cout << M[i][j] << ' ';
+		}
+		std::cout << '\n';
 	}
 }
