6917593dc0ff574d9f318db08a64b1bb4bb53b83
NaimSantos <naimsantos45@gmail.com>
2021-10-05 18:02:20 -0300

    Dados para t = 1000

--- Trabalho 2/output_data/plot_grafico.plt
+++ Trabalho 2/output_data/plot_grafico.plt
@@ -1,9 +1,10 @@
 # Script para plotar os gráficos da distribuição de temperatura na parede fina
-plot "numeric_data_k_59_h_100.txt" using 1:2 title "h = 100" w l lw 2 lc "forest-green", "numeric_data_k_59_h_380.txt" using 1:2 title "h = 380" w l lw 2 lc "blue", "numeric_data_k_59_h_600.txt" using 1:2 title "h = 600" w l lw 2 lc "red"
+plot "Temperatura_t_1000.txt" using 1:2 title "Temperatura" w l lw 2 lc "red"
+#plot "numeric_data_k_59_h_100.txt" using 1:2 title "h = 100" w l lw 2 lc "forest-green", "numeric_data_k_59_h_380.txt" using 1:2 title "h = 380" w l lw 2 lc "blue", "numeric_data_k_59_h_600.txt" using 1:2 title "h = 600" w l lw 2 lc "red"
 set terminal pngcairo
-set output "Grafico_k_fixo.png"
+set output "Grafico_t_100.png"
 set grid
-#set title "Perfil de Temperatura na aleta"
+#set title "Perfil de Temperatura na placa"
 set xlabel "Posição (m)"
 set ylabel "Temperatura ({\260}C)"
 replot
--- Trabalho 2/source/implicit_solver.cpp
+++ Trabalho 2/source/implicit_solver.cpp
@@ -6,7 +6,7 @@
 
 #include <armadillo>
 
-// Variáveis do problemna e do domínio da simulação: :
+// Variáveis do problemna e do domínio da simulação:
 constexpr double kappa{ 0.6 };                            // coeficiente de condutividade térmica
 constexpr double h{ 15.0 };                               // coeficiente de troca de calor por convecção
 constexpr double cp{ 1200.0 };                            // calor específico
@@ -15,7 +15,7 @@ constexpr double g{ 100000.0 };                           // geração interna
 constexpr double T_inf{ 20.0 };                           // temperatura do ambiente
 constexpr double T0{ 20.0 };                              // temperatura inicial da placa
 constexpr double L{ 0.3 };                                // comprimento da placa
-constexpr int N{ 11 };                                    // número de nós da malha
+constexpr int N{ 31 };                                    // número de nós da malha
 constexpr double ti{ 0.0 };                               // tempo inicial da simulação
 constexpr double tf{ 1000.0 };                            // tempo total da simulação
 constexpr auto dx = L / (N - 1);                          // comprimento do intervalo
@@ -26,7 +26,8 @@ constexpr auto r1 = (alpha * dt) / (dx * dx);             // coeficiente r do mé
 constexpr auto eta = 3.0 + ((2 * h * dx) / kappa);
 constexpr auto sigma = (g * dt) / (rho * cp);
 
-void implicit_diff(arma::mat& A, arma::mat& B, const double r);
+void implicit_diff(arma::mat& A, arma::vec& B, const double r);
+void save_data(const arma::vec& Temperature, const arma::vec& Position, const int step);
 
 int main(int argc, char** argv){
 	std::cout << "\ndx = " << dx << ", dt = " << dt << std::endl;
@@ -35,31 +36,18 @@ int main(int argc, char** argv){
 	// Matrizes utilizadas:
 	arma::mat A(N, N, arma::fill::zeros);
 	arma::vec B(N, arma::fill::value(T0));
+	arma::vec X = arma::linspace(0.0, L, N);
 
-	arma::mat X = arma::linspace(0.0, L);
-	X.print("X:");
-
-	B.print("B:");
 	implicit_diff(A, B, r1);
-	B.print("B:");
 
+	save_data(B, X, static_cast<int>(tf));
 	std::cout << "Execution reached the end" << std::endl;
 	std::cin.get();
 	return 0;
 }
 
-void implicit_diff(arma::mat& A, arma::mat& B, const double r){
-
-	std::fstream printer{ "Temperatura_Implicit_Diff.txt", std::ios::out | std::ios::trunc };
-	printer << "Perfil de Temperatura via Solver Implicito.\n";
-	printer << "i t x T\n";
-
-	int step = 0;
-	printer << step << ' ' << ti << ' ';
-	for (double x = 0.0; x <= L; x = x+dx)
-		printer << x << ' ';
-	printer << T0 << '\n';
-
+void implicit_diff(arma::mat& A, arma::vec& B, const double r){
+	std::cout << "Numerical solver started..." << std::endl;
 	// Preenchemos a matriz A (B já foi preenchido com os valores de T0):
 	A(0,0) = -3.0;
 	A(0,1) = 4.0;
@@ -76,7 +64,7 @@ void implicit_diff(arma::mat& A, arma::mat& B, const double r){
 	A(N-1, N-1) = eta;
 
 	// Os passos iterativos do método:
-	for (step = 1; step < nsteps; step++) {
+	for (int step = 1; step < nsteps; step++) {
 		// Corrige B:
 		B(0) = 0.0;
 		for (int i = 1; i < N - 1; i++) {
@@ -85,12 +73,14 @@ void implicit_diff(arma::mat& A, arma::mat& B, const double r){
 		B(N - 1) = (2 * h * dx * T0) / kappa;
 		// Resolve o sistema:
 		B = solve(A, B);
-		printer << ' ' << step * dt << ' ';
-		for (int i = 0; i < N; i++) {
-			printer << B[i] << ' ';
-		}
-		printer << '\n';
-		
 	}
-	printer.close();
+	std::cout << "Numerical solver finished." << std::endl;
+}
+void save_data(const arma::vec& Temperature, const arma::vec& Position, const int step) {
+	std::fstream printer{ "Temperatura_t_" + std::to_string(step) + ".txt", std::ios::out | std::ios::trunc };
+
+	printer << "Pos Temperatura\n";
+	for (int i = 0; i < N; i++) {
+		printer << Position(i) << ' ' << Temperature(i) << '\n';
+	}
 }
