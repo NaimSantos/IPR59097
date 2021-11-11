4142323959759e1a48adc9af81de611fc9ef264f
NaimSantos <naimsantos45@gmail.com>
2021-11-11 11:22:55 -0300

    Preliminar

--- Trabalho 3/source/p2DHeat.py
+++ Trabalho 3/source/p2DHeat.py
@@ -5,7 +5,7 @@ import math
 # Variáveis do problema e do domínio:
 L = 0.1                  # comprimento total da placa ( Lx = Ly)
 N = 25                   # número de nós da malha (considerando N = M)
-tf = 1200                # tempo final da simulação
+tf = 120                # tempo final da simulação
 dx = L/(N-1)             # comprimento do intervalo (dx = dy)
 dt = 0.2                 # passo de tempo
 nsteps = int(tf/dt)      # número de passos de tempo
@@ -14,17 +14,18 @@ rho = 600
 cp = 1200
 h = 15.0
 g = 100000
-T0 = 20.0                # temperatura inicial da placa
+T_ini = 20.0             # temperatura inicial da placa
 T_f = 20.0               # temperatura do fluido na área vazada
 T1 = 100.0               # temperatura nas arestas superior e direita
-alph = kappa/(rho*cp)
-r = (kappa*dt)/(rho*cp*(dx**2))
-gamma = alph*dt/kappa
-mu = (2*h*dx)/kappa
+alpha = kappa/(rho*cp)
+r = (kappa*dt)/(rho*cp*dx*dx)
+lambda = (alpha*g*dt)/kappa
+gamma = 2*h*dx/kappa
+
 
 # Malha e outros arrays:
 tim = np.linspace(0.0, tf, nsteps)   # Tempos
-Temp = np.full((N, N), T0)           # Malha NxN de temperaturas, inicializadas em T0.
+Temp = np.full((N, N), T_ini)           # Malha NxN de temperaturas, inicializadas em T_ini.
 test_array = np.arange(100*100).reshape(100,100)
 
 
@@ -47,12 +48,14 @@ def set_outer_boundaries(Temp):
     # Aresta esquerda:
     for i in range(0, N-1):
         j = 0
-        Temp[i, j] = (1/3)*(4*Temp[i, j+1] - Temp[i, j+2])
-    
+        Temp[i, j] = T1
+        #Temp[i, j] = (1/3)*(4*Temp[i, j+1] - Temp[i, j+2])
+
     # Aresta inferior:
     for j in range(1, N-1):
         i = 0
-        Temp[i, j] = (1/3)*(4*Temp[i+1, j] - Temp[i+2, j])
+        Temp[i, j] = T1
+        #Temp[i, j] = (1/3)*(4*Temp[i+1, j] - Temp[i+2, j])
 
     # Aresta superior:
     for i in range (0, N):
@@ -63,11 +66,13 @@ def set_outer_boundaries(Temp):
     for j in range(0, N):
         i = N-1
         Temp[i, j] = T1
-        
+ 
 
 
 
 def solve_explicitly(Temp):
+    p = int(0.3*N)
+    p_w = int(0.7*N)
 
     # Iteração no tempo:
     t_current = 0.0
@@ -81,7 +86,28 @@ def solve_explicitly(Temp):
 
             # Iteração em y (de 1 a N-2):
             for j in range (1, N-1):
-               Temp[i, j] = 0.0
+
+                # Temperatura na região vazada
+                if ((i>p and i<p_w) and (j>p and j<p_w)):
+                    Temp[i, j] = T_f
+
+                # Temperatura nas regiões do contorno interno (área vazada)
+                elif (i==p+1 and (j>p and j<p_w)):
+                    Temp[i, j] = (1/(3 + gamma))*(gamma*T_ini + 4*Temp[i-1, j] - Temp[i-2, j] )
+
+                elif (i==p_w-1 and (j>p and j<p_w)):
+                    Temp[i, j] = (1/(gamma - 3))*(gamma*T_ini - 4*Temp[i+1, j] + Temp[i+2, j] )
+
+                elif (j==p+1 and (i>=p and i<=p_w)):
+                    Temp[i, j] = (1/(3 + gamma))*(gamma * T_ini + 4*Temp[i, j-1] - Temp[i, j-2] )
+
+                elif(j==p_w-1 and (i>=p and i<=p_w)):
+                    Temp[i, j] = (1/(gamma - 3))*(gamma*T_ini - 4*Temp[i, j+1] + Temp[i, j+2])
+
+                # Temperatura nos demais nós
+                else:
+                    Temp[i, j] = Temp[i, j] + r*(Temp[i+1, j] + Temp[i-1, j] + Temp[i, j+1] + Temp[i, j-1] - 4*Temp[i, j]) + lambda
+
         # Próximo passo de tempo
         t_current = t_current + dt
 
